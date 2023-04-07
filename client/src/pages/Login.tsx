15024439ba402e523a7c9a36cab9b6934008babb
thinhtethtetzaw <thinhtethtetzaw778@gmail.com>
2023-04-07 23:52:02 +0630

    login folder structure

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import About from "@/components/organisms/login/About";
+import Form from "@/components/organisms/login/Form";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex h-screen flex-col lg:flex-row">
+      <About />
+      <Form />
+    </div>
+  );
 };
 
 export default Login;
