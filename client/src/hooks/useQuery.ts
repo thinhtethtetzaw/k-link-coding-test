1bda832497a529f733c2f8f5fa2a35513ba7069f
thinhtethtetzaw <thinhtethtetzaw778@gmail.com>
2023-04-09 17:12:48 +0630

    update

--- client/src/hooks/useQuery.ts
+++ client/src/hooks/useQuery.ts
@@ -4,7 +4,8 @@ import { useQuery, useMutation } from "@tanstack/react-query";
 import { loginState } from "@/recoil/state";
 
 // const NODE_SERVER = "http://localhost:8081/api/v1";
-const NODE_SERVER = "https://k-link-coding-test-production.up.railway.app";
+const NODE_SERVER =
+  "https://k-link-coding-test-production.up.railway.app/api/v1";
 
 const config = {
   headers: {
